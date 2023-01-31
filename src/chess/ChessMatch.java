78696a17038470ad2ab9ee467127765302b5ec49
aggsdp <aggsdpz@gmail.com>
2023-01-30 21:39:11 -0300

    movimentos possiveis da torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
 
         return (ChessPiece) capturedPiece;
@@ -48,6 +49,12 @@ public class ChessMatch {
             throw new ChessException("Não existem movimentos possíveis para a peça escolhida.");
         }
 
+    }
+
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece(source).possibleMove(target)){
+            throw new ChessException("A peça escolhida não pode se mover para posição de destino.");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
